4140c0adb7937d1bb2d7d4f0883713ae96a3e0d1
qqgameover <kasper35@live.no>
2021-10-17 02:14:23 +0200

    x

---
+++ BossFight/GameChar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossFight
+{
+    class GameChar
+    {
+        public string Name { get; set; }
+        public int Health { get; private set; }
+        public int Strength { get; set; }
+        public int Stamina { get; private set; }
+
+        public GameChar(string name ,int health, int strength, int stamina)
+        {
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            Name = name;
+        }
+
+        public void Fight(GameChar target)
+        {
+            target.Health -= Strength;
+            Stamina -= 10;
+            Console.WriteLine($"{Name} attacked {target.Name} for {Strength} damage, the targets HP is now {target.Health}");
+        }
+
+        public void Rest()
+        {
+            Stamina += 10;
+            Console.WriteLine($"The {Name} is resting, their stamina is now: {Stamina}");
+        }
+    }
+}
--- BossFight/Program.cs
+++ BossFight/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading;
 
 namespace BossFight
 {
     class Program
     {
+        private static readonly Random rng = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var heroGame = new GameChar("Hero", 100, 20, 40);
+            var bossGame = new GameChar("Boss", 400, 30, 10);
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "fight")
+                {
+                    heroGame.Fight(bossGame);
+                }
+                if (command == "rest")
+                {
+                    heroGame.Rest();
+                }
+                Thread.Sleep(500);
+
+                if (bossGame.Stamina >= 10)
+                {
+                    bossGame.Strength = rng.Next(0, 30);
+                    bossGame.Fight(heroGame);
+                    continue;
+                }
+                bossGame.Rest();
+            }
         }
     }
 }
