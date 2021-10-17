082aa3b69fc719e3616d07ce21744fd604104015
qqgameover <kasper35@live.no>
2021-10-17 18:00:55 +0200

    x

--- BossFight/Program.cs
+++ BossFight/Program.cs
@@ -14,20 +14,30 @@ namespace BossFight
             while (true)
             {
                 var command = Console.ReadLine();
-                if (command == "fight")
+                if (command == "fight" && heroGame.Stamina >= 10)
                 {
                     heroGame.Fight(bossGame);
                 }
-                if (command == "rest")
+                else
                 {
                     heroGame.Rest();
                 }
                 Thread.Sleep(500);
+                if (bossGame.Health <= 0)
+                {
+                    Console.WriteLine("The hero wins! The boss has been defeated");
+                    break;
+                }
 
                 if (bossGame.Stamina >= 10)
                 {
                     bossGame.Strength = rng.Next(0, 30);
                     bossGame.Fight(heroGame);
+                    if (heroGame.Health <= 0)
+                    {
+                        Console.WriteLine("The boss wins! The hero has been defeated");
+                        break;
+                    }
                     continue;
                 }
                 bossGame.Rest();
